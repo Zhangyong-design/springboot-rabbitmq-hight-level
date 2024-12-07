cc51f3484f40078710aaf7692603a598c522990c
Edward <8606045+zhang-yong188@user.noreply.gitee.com>
2024-12-07 14:13:38 +0800

    error message 配置

--- consumer/src/main/java/com/zy/errordeal/ErrorMessageConfig.java
+++ consumer/src/main/java/com/zy/errordeal/ErrorMessageConfig.java
@@ -7,7 +7,6 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.retry.MessageRecoverer;
 import org.springframework.amqp.rabbit.retry.RepublishMessageRecoverer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
